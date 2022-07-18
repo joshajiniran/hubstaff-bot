3dbc8e8272a81ce5cd4072aa83ac913a6cc45e7a
Josh Ajiniran <josuajiniran@gmail.com>
2022-07-18 07:25:15 +0100

    initial application

---
+++ main.py
@@ -0,0 +1,28 @@
+import pyautogui
+
+"""
+When the scripts starts running, check if the browser, code editor and postman is running, otherwise launch them
+You want to automate mouse movement at intervals: scroll, click
+You want to alternate between code and browser or any other app (postman, terminal etc) at intervals
+You want to visit websites (usually stackoverflow, or sites that show you are working)
+
+For more advance/complex automation, you want to copy an entire code and simulate typing it in the editor
+toml/json config would hold configurations for 
+"""
+
+def automate_mouse():
+    pass
+
+
+def automate_keypress():
+    pass
+
+def automate_browsing():
+    pass
+
+def start_application(*args):
+    pass
+
+
+while True:
+    pyautogui.scroll()
